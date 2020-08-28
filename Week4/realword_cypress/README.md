4486b59f11239bab88276af0750078ae5a098fc4
mayqf <mayqf@mail.missouri.edu>
2020-08-28 13:26:24 +0200

    w4

---
+++ Week4/realword_cypress/README.md
@@ -0,0 +1,35 @@
+# Cypress io Assignment
+
+For setting up the environment to execute this test existed in this repo, please run the following commands:
+
+**clone this repo to a local directory (HTTP)**
+
+git clone https://github.com/mayqf/QA_Training/tree/week4_mustafa/Week4/realword_cypress
+
+**cd into the cloned repo**
+
+cd realworld_cypress
+
+**install the node_modules**
+
+npm install
+
+**open cypress test runner tool**
+
+npx cypress open
+
+**click on the title of the wanted test for execution**
+
+**the browser selected by the test runner tool will be opened and start executing the test**
+
+---
+
+**Test cases implemented (Happy Path Scenarios):**
+1.  Login with valid credentials.
+2.  Login with invalid credentials.
+3.  Login with empty data.
+4.  Check Need an account? link.
+
+
+
+
---
+++ Week4/realword_cypress/cypress/integration/login.spec.js
@@ -0,0 +1,38 @@
+describe('Realworld Login Scenarios with Cypress io', () => {
+    beforeEach(() => {
+        cy.visit('/')
+        cy.contains('Sign in').click()
+      });
+
+    it('Login with valid credentials', () => {
+        cy.get('[placeholder="Email"]').type('hyf_tester@gmail.com')
+        cy.get('[placeholder="Password"]').type('HYFtester1')
+        cy.get('button[type="submit"]').click()
+        cy.wait(4000)
+        
+    });
+
+    it('Login with invalid credentials', () => {
+        cy.get('[placeholder="Email"]').type('hyf_test@gmail.com')
+        cy.get('[placeholder="Password"]').type('HYFtester1')
+        cy.get('button[type="submit"]').click()
+        //cy.title().should('include', 'email or password is invalid')
+        cy.wait(4000)
+        
+    });
+
+    it('Login with empty data', () => {
+        cy.get('[placeholder="Email"]').invoke('val', '')
+        cy.get('[placeholder="Password"]').invoke('val', '')
+        cy.get('button[type="submit"]').click()
+        //cy.get('.error').should('contain', 'email or password is invalid')
+        cy.wait(4000)
+        
+    });
+
+    it('Check Need an account? link', () => {
+        cy.contains('Need an account?').click()
+        
+    });
+
+});
